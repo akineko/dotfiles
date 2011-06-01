ef7ffaf80f3f310f4e5f531bdd8b3dafd8d47911
akineko <akineko.mail@gmail.com>
2011-06-01 13:41:54 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cp -r .vim       $HOME/
+cp    .vimrc     $HOME/
+cp    .gvimrc    $HOME/
+cp    .zshrc     $HOME/
+cp    .tmux.conf $HOME/
+cp    .gitconfig $HOME/
+cp    .hgrc      $HOME/
+cp    .gdbinit   $HOME/
+cp    .bashrc    $HOME/
+
