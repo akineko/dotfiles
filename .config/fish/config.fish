c97047d70cf10557965762da5b694414c826b4fa
akineko <akineko.mail@gmail.com>
2017-02-05 20:09:34 +0000

    [fish]zshから移植漏れしていた環境変数を追加する

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,15 @@
+set -x TERM xterm-256color
+
+# Subversion
+set -x SVN_EDITOR nvim
+
+# Mercurial
+set -x HGENCODING utf8
+
+# XDG_CONFIG
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_CACHE_HOME $HOME/.cache
+
 # anyenv
 # set -x PATH $HOME/.anyenv/bin $PATH
 # eval (anyenv init - fish)
