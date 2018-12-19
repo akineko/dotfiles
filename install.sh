ac9e8686bf8621c27758570a808fcc235cf6e990
akineko <akineko.mail@gmail.com>
2018-12-19 05:42:42 +0000

    add force option

--- install.sh
+++ install.sh
@@ -3,30 +3,30 @@ set -Ceu
 
 abspath=$(cd $(dirname $0) && pwd)
 
-ln -s $abspath/.vimrc     $HOME/.vimrc
-ln -s $abspath/.gvimrc    $HOME/.gvimrc
-ln -s $abspath/.zshrc     $HOME/.zshrc
-ln -s $abspath/.zsh       $HOME/.zsh
-ln -s $abspath/.tmux.conf $HOME/.tmux.conf
-ln -s $abspath/.gitconfig $HOME/.gitconfig
-ln -s $abspath/.tigrc     $HOME/.tigrc
-ln -s $abspath/.hgrc      $HOME/.hgrc
-ln -s $abspath/.gdbinit   $HOME/.gdbinit
-ln -s $abspath/.bashrc    $HOME/.bashrc
+ln -sf $abspath/.vimrc     $HOME/.vimrc
+ln -sf $abspath/.gvimrc    $HOME/.gvimrc
+ln -sf $abspath/.zshrc     $HOME/.zshrc
+ln -sf $abspath/.zsh       $HOME/.zsh
+ln -sf $abspath/.tmux.conf $HOME/.tmux.conf
+ln -sf $abspath/.gitconfig $HOME/.gitconfig
+ln -sf $abspath/.tigrc     $HOME/.tigrc
+ln -sf $abspath/.hgrc      $HOME/.hgrc
+ln -sf $abspath/.gdbinit   $HOME/.gdbinit
+ln -sf $abspath/.bashrc    $HOME/.bashrc
 
 mkdir -p $HOME/.cache
 mkdir -p $HOME/.config
-ln -s $abspath/.config/nvim $HOME/.config/nvim
+ln -sf $abspath/.config/nvim $HOME/.config/nvim
 
 mkdir -p $HOME/.config/fish
-ln -s $abspath/.config/fish/config.fish $HOME/.config/fish/config.fish
-ln -s $abspath/.config/fish/fishfile    $HOME/.config/fish/fishfile
+ln -sf $abspath/.config/fish/config.fish $HOME/.config/fish/config.fish
+ln -sf $abspath/.config/fish/fishfile    $HOME/.config/fish/fishfile
 
 mkdir -p $HOME/.config/fish/conf.d
-ln -s $abspath/.config/fish/conf.d/000-env.fish $HOME/.config/fish/conf.d/000-env.fish
+ln -sf $abspath/.config/fish/conf.d/000-env.fish $HOME/.config/fish/conf.d/000-env.fish
 
 mkdir -p $HOME/.config/fish/functions
-ln -s $abspath/.config/fish/functions/fish_prompt.fish $HOME/.config/fish/functions/fish_prompt.fish
+ln -sf $abspath/.config/fish/functions/fish_prompt.fish $HOME/.config/fish/functions/fish_prompt.fish
 
 cp -r .vim       $HOME/
 mkdir -p $HOME/vimbackup
