e785bdfb30f2df2f13fa6a0f3472b2e19e8c3d77
akineko <akineko.mail@gmail.com>
2019-04-09 17:37:36 +0000

    [gotools] dep を一旦コメントアウトする

--- get-gotools.sh
+++ get-gotools.sh
@@ -11,7 +11,7 @@ go get github.com/direnv/direnv
 go get github.com/mattn/files
 
 # golang
-go get github.com/golang/dep/cmd/dep
+# go get github.com/golang/dep/cmd/dep
 go get golang.org/x/tools/cmd/godoc
 go get github.com/motemen/gore
 go get github.com/k0kubun/pp
