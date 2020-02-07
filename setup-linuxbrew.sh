6d8f14490a97c3b75d13bee7e0e9d003f9e796a9
akineko <akineko.mail@gmail.com>
2020-02-07 05:12:04 +0000

    [linuxbrew]brewの一括インストールをシェルスクリプト化する

---
+++ setup-linuxbrew.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# brew tap linuxbrew/xorg
+
+# tap "delphinus/sfmono-square"
+# brew "delphinus/sfmono-square/sfmono-square"
+
+# grep tools
+brew install ripgrep
+brew install the_platinum_searcher
+brew install the_silver_searcher
+
+# C++ build tools
+brew install cmake
+brew install meson
+brew install ninja
+
+# kubernetes
+brew install kubernetes-cli
+brew install kubectx
+brew install stern
+
+# terraform
+brew install terraformer
+brew install tfenv
+
+# shell
+brew install fish
+brew install zsh
+brew install starship
+brew install tmux
+
+# cli tools
+brew install anyenv
+brew install bat
+brew install exa
+brew install fd
+brew install fselect
+brew install fzf
+brew install gibo
+brew install jq
+brew install lsd
+brew install procs
+brew install rargs
+
