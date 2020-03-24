d77aa70cac973da2b063db02f0d56f969e05a0fe
akineko <akineko.mail@gmail.com>
2020-03-24 17:39:37 +0900

    vim-goで入れていたgo開発時に利用するツール群をスクリプト化する

--- setup-gotools.sh
+++ setup-gotools.sh
@@ -9,7 +9,6 @@ go get -u github.com/direnv/direnv
 
 # vim
 go get -u github.com/mattn/files
-go get -u golang.org/x/tools/gopls
 
 # golang
 # go get github.com/golang/dep/cmd/dep
@@ -18,4 +17,24 @@ go get -u github.com/motemen/gore
 go get -u github.com/k0kubun/pp
 go get -u github.com/motemen/gofind/cmd/gofind
 go get -u github.com/clipperhouse/gen
+
+# from vim-go
+go get -u github.com/klauspost/asmfmt/cmd/asmfmt
+go get -u github.com/go-delve/delve/cmd/dlv
+go get -u github.com/kisielk/errcheck
+go get -u github.com/davidrjenni/reftools/cmd/fillstruct
+go get -u github.com/rogpeppe/godef
+go get -u github.com/zmb3/gogetdoc
+go get -u golang.org/x/tools/cmd/goimports
+go get -u golang.org/x/lint/golint
+go get -u golang.org/x/tools/gopls
+go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
+go get -u github.com/fatih/gomodifytags
+go get -u golang.org/x/tools/cmd/gorename
+go get -u github.com/jstemmer/gotags
+go get -u golang.org/x/tools/cmd/guru
+go get -u github.com/josharian/impl
+go get -u honnef.co/go/tools/cmd/keyify
+go get -u github.com/fatih/motion
+go get -u github.com/koron/iferr
 
