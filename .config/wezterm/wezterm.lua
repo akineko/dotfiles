89d0e31e76b74b3ca47e281006ffa7eb6f5895f0
akineko <akineko.mail@gmail.com>
2022-06-05 23:57:49 +0900

    add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm';
+return {
+  font = wezterm.font("HackGenNerd Console"),
+  font_size = 11,
+  color_scheme = "Ayu Mirage",
+--   color_scheme = "Dracula+",
+--   color_scheme = "Japanesque",
+--   color_scheme = "lovelace",
+--   color_scheme = "OneHalfDark",
+}
