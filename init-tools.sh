4689f30390140a4a0f59aab60cbf5dd7938bdefa
akineko <akineko.mail@gmail.com>
2022-06-18 02:14:56 +0900

    brewとvoltaのインストールを追加する

--- init-tools.sh
+++ init-tools.sh
@@ -10,3 +10,10 @@ if [ ! -d ~/.zinit/bin ]; then
   git clone https://github.com/zdharma-continuum/zinit.git ~/.zinit/bin
 fi
 
+if ! (type brew &>/dev/null); then
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if ! (type volta &>/dev/null); then
+  curl https://get.volta.sh | bash -s -- --skip-setup
+fi
