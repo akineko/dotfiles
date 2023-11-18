f40eba105fde3d5d155dff17297d482591ae2109
akineko <akineko.mail@gmail.com>
2023-11-18 13:40:55 +0000

    [vim] fzf.vimで行っていた操作をtelescopeへ統合する

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -207,7 +207,7 @@ vim.keymap.set("n", "<Space>.l", ":source $XDG_CONFIG_HOME/nvim/init.lua<CR>")
 -- 選択した文字列の検索/置換
 vim.keymap.set("v", "//", [[y/<C-r>=escape(@", '\\/.*$^~[]')<CR><CR>]], { silent = true })
 vim.keymap.set("v", "/r", [[y:%s/\v<C-r>=escape(@", '\\/.*$^~[]')<CR>//gc<Left><Left><Left>]])
-vim.keymap.set("v", "/g", [[y:Rg <C-r>=escape(@", '\\/.*$^~[]')<CR><CR>]])
+vim.keymap.set("v", "/g", [[y:Telescope grep_string search=<C-r>=escape(@", '\\/.*$^~[]')<CR><CR>]])
 
 -- ウィンドウ操作
 vim.keymap.set("n", "<Space>h", "<C-w>h")
--- .config/nvim/lua/lazy-plugins/misc.lua
+++ .config/nvim/lua/lazy-plugins/misc.lua
@@ -103,15 +103,15 @@ return {
     init = function()
       vim.cmd([[
         let g:precious_enable_switchers = {
-            \	"*" : {
-            \		"setfiletype" : 0
-            \	},
-            \	"help" : {
-            \		"setfiletype" : 1
-            \	},
-            \	"toml" : {
-            \		"setfiletype" : 1
-            \	},
+            \ "*" : {
+            \   "setfiletype" : 0
+            \ },
+            \ "help" : {
+            \   "setfiletype" : 1
+            \ },
+            \ "toml" : {
+            \   "setfiletype" : 1
+            \ },
             \}
       ]])
     end,
@@ -122,61 +122,3 @@ return {
     event = 'BufEnter',
   },
 }
-  -- {
-  --   'mattn/webapi-vim',
-  -- },
-  -- {
-  --   'osyo-manga/vim-anzu',
-  --   hook_add = '''
-  --   nmap n <Plug>(anzu-n-with-echo)
-  --   nmap N <Plug>(anzu-N-with-echo)
-  --   nmap * <Plug>(anzu-star-with-echo)
-  --   nmap # <Plug>(anzu-sharp-with-echo)
-  --   nmap <Esc><Esc> <Plug>(anzu-clear-search-status)
-  --
-  --   " statusline
-  --   if exists('*anzu#search_status')
-  --     set statusline=%{anzu#search_status()}
-  --   endif
-  --   '''
-  -- },
-  -- {
-  --   'junegunn/fzf.vim',
-  --   dependencies = { 'junegunn/fzf' },
-  --   hook_add = '''
-  --   command! -bang -nargs=* Rg
-  --     \ call fzf#vim#grep(
-  --     \   'rg --column --line-number --no-heading --color=always --smart-case '.shellescape(<q-args>), 1,
-  --     \   <bang>0 ? fzf#vim#with_preview('up:60%')
-  --     \           : fzf#vim#with_preview({'options': '--exact --reverse --delimiter : --nth 3..'}, 'right:50%:hidden', '?'),
-  --     \   <bang>0)
-  --
-  --   function! s:find_git_root()
-  --     return system('git rev-parse --show-toplevel 2> /dev/null')[:-2]
-  --   endfunction
-  --   command! ProjectFiles execute 'Files' s:find_git_root()
-  --
-  --   function! s:build_quickfix_list(lines)
-  --     call setqflist(map(copy(a:lines), '{ "filename": v:val }'))
-  --     copen
-  --     cc
-  --   endfunction
-  --
-  --   let g:fzf_action = {
-  --     \ 'ctrl-q': function('s:build_quickfix_list'),
-  --     \ 'ctrl-t': 'tab split',
-  --     \ 'ctrl-x': 'split',
-  --     \ 'ctrl-v': 'vsplit' }
-  --
-  --   let $FZF_DEFAULT_OPTS = '--bind alt-a:select-all,alt-d:deselect-all'
-  --
-  --   " nnoremap [fzf] <Nop>
-  --   " nmap <Leader>f [fzf]
-  --   " nnoremap [fzf]f :<C-u>Files<CR>
-  --   " nnoremap [fzf]b :<C-u>Buffers<CR>
-  --   " nnoremap [fzf]t :<C-u>Tags<CR>
-  --
-  --   " nnoremap <silent> <C-p> :ProjectFiles<CR>
-  --   " nnoremap <silent> <C-p> :GFiles<CR>
-  --   '''
-  -- },
