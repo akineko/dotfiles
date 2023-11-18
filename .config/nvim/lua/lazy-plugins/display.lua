c4a1d1f32f047d1ef6458122b02869eab7cc54e6
akineko <akineko.mail@gmail.com>
2023-11-18 15:34:07 +0000

    [vim] add indent-blankline.nvim

--- .config/nvim/lua/lazy-plugins/display.lua
+++ .config/nvim/lua/lazy-plugins/display.lua
@@ -12,21 +12,39 @@ return {
   {
     'HiPhish/rainbow-delimiters.nvim',
     event = 'BufEnter',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter', },
     main = 'rainbow-delimiters.setup',
-    opts = function()
-      local rainbow_delimiters = require('rainbow-delimiters')
-      return {
-        highlight = {
-            'TSRainbowRed',    -- guifg=#bf616a
-            'TSRainbowYellow', -- guifg=#ebcb8b
-            'TSRainbowBlue',   -- guifg=#81a1c1
-            'TSRainbowOrange', -- guifg=#c9826b
-            'TSRainbowGreen',  -- guifg=#a3be8c
-            'TSRainbowViolet', -- guifg=#b48ead
-            'TSRainbowCyan',   -- guifg=#88c0d0
-        },
+    opts = {
+      highlight = {
+        'TSRainbowRed',    -- guifg=#bf616a
+        'TSRainbowYellow', -- guifg=#ebcb8b
+        'TSRainbowBlue',   -- guifg=#81a1c1
+        'TSRainbowOrange', -- guifg=#c9826b
+        'TSRainbowGreen',  -- guifg=#a3be8c
+        'TSRainbowViolet', -- guifg=#b48ead
+        'TSRainbowCyan',   -- guifg=#88c0d0
       }
-    end,
+    },
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = 'BufEnter',
+    main = "ibl",
+    opts = {
+      scope = {
+        highlight = {
+          'TSRainbowRed',    -- guifg=#bf616a
+          'TSRainbowYellow', -- guifg=#ebcb8b
+          'TSRainbowBlue',   -- guifg=#81a1c1
+          'TSRainbowOrange', -- guifg=#c9826b
+          'TSRainbowGreen',  -- guifg=#a3be8c
+          'TSRainbowViolet', -- guifg=#b48ead
+          'TSRainbowCyan',   -- guifg=#88c0d0
+        },
+        show_start = false,
+        show_end = false,
+      },
+    },
   },
 }
+
