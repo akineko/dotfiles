4833ea8d160557be1563abf08bd24585c94bf739
akineko <akineko.mail@gmail.com>
2023-11-18 13:54:32 +0000

    [vim] rainbow-delimiters.nvimの設定をdisplayへ

--- .config/nvim/lua/lazy-plugins/display.lua
+++ .config/nvim/lua/lazy-plugins/display.lua
@@ -9,5 +9,33 @@ return {
     event = 'BufEnter',
     opts = {},
   },
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    event = 'BufEnter',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    main = 'rainbow-delimiters.setup',
+    opts = function()
+      local rainbow_delimiters = require('rainbow-delimiters')
+      return {
+        strategy = {
+            [''] = rainbow_delimiters.strategy['global'],
+            commonlisp = rainbow_delimiters.strategy['local'],
+        },
+        query = {
+            [''] = 'rainbow-delimiters',
+            lua = 'rainbow-blocks',
+        },
+        highlight = {
+            'TSRainbowRed',    -- guifg=#bf616a
+            'TSRainbowYellow', -- guifg=#ebcb8b
+            'TSRainbowBlue',   -- guifg=#81a1c1
+            'TSRainbowOrange', -- guifg=#c9826b
+            'TSRainbowGreen',  -- guifg=#a3be8c
+            'TSRainbowViolet', -- guifg=#b48ead
+            'TSRainbowCyan',   -- guifg=#88c0d0
+        },
+        -- blacklist = {'c', 'cpp'},
+      }
+    end,
+  },
 }
-
--- .config/nvim/lua/lazy-plugins/treesitter.lua
+++ .config/nvim/lua/lazy-plugins/treesitter.lua
@@ -29,32 +29,6 @@ return {
       'windwp/nvim-ts-autotag',
       'romgrk/nvim-treesitter-context',
       'JoosepAlviste/nvim-ts-context-commentstring',
-      {
-        'HiPhish/rainbow-delimiters.nvim',
-        opts = function()
-          local rainbow_delimiters = require('rainbow-delimiters')
-          return {
-            strategy = {
-                [''] = rainbow_delimiters.strategy['global'],
-                commonlisp = rainbow_delimiters.strategy['local'],
-            },
-            query = {
-                [''] = 'rainbow-delimiters',
-                lua = 'rainbow-blocks',
-            },
-            highlight = {
-                'TSRainbowRed',    -- guifg=#bf616a
-                'TSRainbowYellow', -- guifg=#ebcb8b
-                'TSRainbowBlue',   -- guifg=#81a1c1
-                'TSRainbowOrange', -- guifg=#c9826b
-                'TSRainbowGreen',  -- guifg=#a3be8c
-                'TSRainbowViolet', -- guifg=#b48ead
-                'TSRainbowCyan',   -- guifg=#88c0d0
-            },
-            -- blacklist = {'c', 'cpp'},
-          }
-        end,
-      },
     },
   },
 }
