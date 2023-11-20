7d3f779de188d9281f5b4a015345941e33d0e30b
akineko <akineko.mail@gmail.com>
2023-11-20 16:19:39 +0000

    [vim] add nvim-treesitter-textobjects

--- .config/nvim/lua/lazy-plugins/treesitter.lua
+++ .config/nvim/lua/lazy-plugins/treesitter.lua
@@ -25,9 +25,38 @@ return {
           enable = true,
           enable_autocmd = false,
         },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            },
+            selection_modes = {
+              ['@parameter.outer'] = 'v', -- charwise
+              ['@function.outer'] = 'V', -- linewise
+              ['@class.outer'] = '<c-v>', -- blockwise
+            },
+            include_surrounding_whitespace = true,
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<leader>ta"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<leader>tA"] = "@parameter.inner",
+            },
+          },
+        },
       })
     end,
     dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
       'RRethy/vim-illuminate',
       'RRethy/nvim-treesitter-endwise',
       'windwp/nvim-ts-autotag',
