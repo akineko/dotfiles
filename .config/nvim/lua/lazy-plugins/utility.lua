61ab37cda050fa60ee39ba4964c04cd06b942a9e
akineko <akineko.mail@gmail.com>
2023-11-25 10:56:13 +0000

    [vim] キーバインド関係のプラグインを専用ファイルへ

--- .config/nvim/lua/lazy-plugins/utility.lua
+++ .config/nvim/lua/lazy-plugins/utility.lua
@@ -1,57 +1,5 @@
 return {
   {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 500
-    end,
-    opts = {},
-  },
-  {
-    'anuvyklack/hydra.nvim',
-    event = 'VeryLazy',
-    config = function()
-      local hydra = require('hydra')
-      hydra({
-        name = 'Tab',
-        config = {
-          invoke_on_body = true,
-          timeout = true,
-          color = 'pink',
-        },
-        body = '<C-t>',
-        heads = {
-          { "n", ":tabnew<CR>", { exit = true } },
-          { "c", ":tabclose<CR>" },
-          { "h", ":-tabmove<CR>" },
-          { "l", ":+tabmove<CR>" },
-          { "j", ":tabnext<CR>" },
-          { "k", ":tabprevious<CR>" },
-        },
-      })
-      hydra({
-        name = 'Window',
-        config = {
-          invoke_on_body = true,
-          timeout = true,
-          color = 'pink',
-        },
-        body = '<C-w>',
-        heads = {
-          { "h", "<C-w>h" },
-          { "j", "<C-w>j" },
-          { "l", "<C-w>l" },
-          { "k", "<C-w>k" },
-          { "<", "<C-w><" },
-          { ">", "<C-w>>" },
-          { "+", "<C-w>+" },
-          { "-", "<C-w>-" },
-        },
-      })
-    end,
-  },
-  {
     'nvim-pack/nvim-spectre',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     keys = {
