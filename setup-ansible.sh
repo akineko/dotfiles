6cfe73ec4fa36f0a3d6fa0786ba30dc27314a98c
akineko <akineko.mail@gmail.com>
2024-02-13 14:08:17 +0000

    update setup ansible script

--- setup-ansible.sh
+++ setup-ansible.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -Ceu
 
-sudo apt-get -y install git
-
-sudo apt-get -y install software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get -y install ansible
+sudo apt-get -y install git software-properties-common
+brew install ansible
 
 cd provisioning
 ansible-playbook -i hosts development.yml -K
