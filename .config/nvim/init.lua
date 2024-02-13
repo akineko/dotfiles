8f484bcfee83d0b978e128ec510f1d3b980f5284
akineko <akineko.mail@gmail.com>
2024-02-13 17:50:38 +0000

    [vim] neovim v0.10 で削除されたtermencodingの設定を削除

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,7 +5,6 @@ vim.opt.background = "dark"
 -- encoding
 vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
-vim.opt.termencoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 vim.opt.fileencodings = {
   "ucs-bom",
