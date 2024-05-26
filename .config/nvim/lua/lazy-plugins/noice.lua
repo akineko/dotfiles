75de827338c443a3ed88131464462d553d0c04ce
akineko <akineko.mail@gmail.com>
2024-05-26 15:42:37 +0000

    [vim] noiceのファイル保存時のメッセージを非表示へ

--- .config/nvim/lua/lazy-plugins/noice.lua
+++ .config/nvim/lua/lazy-plugins/noice.lua
@@ -34,22 +34,22 @@ return {
           },
         },
         presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
+          bottom_search = true,         -- use a classic bottom cmdline for search
+          command_palette = true,       -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
+          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false,       -- add a border to hover docs and signature help
         },
-        -- routes = {
-        --   {
-        --     filter = {
-        --       event = "lsp",
-        --       kind = "progress",
-        --       find = "code_action",
-        --     },
-        --     opts = { skip: true },
-        --   },
-        -- },
+        routes = {
+          {
+            filter = {
+              event = "msg_show",
+              kind = "",
+              find = "written",
+            },
+            opts = { skip = true },
+          },
+        },
       })
     end,
   },
