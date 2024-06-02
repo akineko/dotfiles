99ec85cbdbf43b045c06037953f1e515645d0c8e
akineko <akineko.mail@gmail.com>
2024-06-03 02:24:22 +0900

    [setup]voltaのインストールをコメントアウト

--- init-tools.sh
+++ init-tools.sh
@@ -14,6 +14,6 @@ if ! (type brew &>/dev/null); then
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if ! (type volta &>/dev/null); then
-  curl https://get.volta.sh | bash -s -- --skip-setup
-fi
+# if ! (type volta &>/dev/null); then
+#   curl https://get.volta.sh | bash -s -- --skip-setup
+# fi
