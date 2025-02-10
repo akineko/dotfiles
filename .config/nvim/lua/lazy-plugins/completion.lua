676116cd2995c7a70c70129e8877bae0cc14488c
akineko <akineko.mail@gmail.com>
2025-02-11 03:36:37 +0900

    [vim] add codecompanion.nvim

--- .config/nvim/lua/lazy-plugins/completion.lua
+++ .config/nvim/lua/lazy-plugins/completion.lua
@@ -105,5 +105,64 @@ return {
       )
     end,
   },
+  {
+    'olimorris/codecompanion.nvim',
+    event = { 'VeryLazy' },
+    keys = {
+      { '<leader>aa', ':CodeCompanionActions<CR>',  mode = { 'n', 'v' } },
+      { '<leader>ac', ':CodeCompanionChat<CR>' },
+      { '<leader>ai', ':CodeCompanion /buffer<CR>', mode = { 'n', 'v' } },
+    },
+    opts = {
+      adapter = {
+        copilot = function()
+          return require('codecompanion.adapters').extend('copilot', {
+            schema = {
+              model = {
+                default = 'claude-3.5-sonnet',
+              },
+            },
+          })
+        end,
+      },
+      strategies = {
+        chat = {
+          adapter = 'copilot',
+          slash_commands = {
+            ['buffer'] = {
+              opts = {
+                provider = 'telescope',
+              },
+            },
+            ['file'] = {
+              opts = {
+                provider = 'telescope',
+              },
+            },
+            ['help'] = {
+              opts = {
+                provider = 'telescope',
+              },
+            },
+            ['symbols'] = {
+              opts = {
+                provider = 'telescope',
+              },
+            },
+          },
+        },
+        inline = {
+          adapter = 'copilot',
+        },
+      },
+      display = {
+        action_palette = {
+          provider = 'telescope',
+        },
+      },
+      opts = {
+        language = 'Japanese',
+      },
+    },
+  },
 }
-
